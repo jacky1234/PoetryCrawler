57e6caec1430ce2ad1b9e5dfdc06900885bf2231
YC <jackdoson2011@gmail.com>
2023-10-24 08:55:25 +0800

    2023年10月24日 星期二 08时55分25秒 CST refactor

--- gushiwen/gushiwen/pipelines.py
+++ gushiwen/gushiwen/pipelines.py
@@ -6,57 +6,71 @@
 # See: https://doc.scrapy.org/en/latest/topics/item-pipeline.html
 
 import json
-import MySQLdb
+import pymysql
+
+
 class GushiwenPipeline(object):
-	content=[]
-	def open_spider(self,spider):
-		self.f=open("style.json",'w')
-	def close_spider(self,spider):
-		json.dump(self.content,fp=self.f,indent=4)
-		self.f.close()
-	def process_item(self,item,spider):
-		"""
-		#数据库请用自己的地址替换
-		con=MySQLdb.connect(host='xx.xx.xx.xx',user='xx',passwd='xx',db='xx',port=xx,charset='utf8')
-		cur=con.cursor()
-		cur.execute('insert into Tag values(%s,%s)',("形式",item['style']))
-		con.commit()
-		con.close()
-		print("style",item['style'])
-		self.content.append(item)
-		"""
-		return item
+    content = []
+
+    def open_spider(self, spider):
+        self.f = open("style.json", 'w')
+
+    def close_spider(self, spider):
+        json.dump(self.content, fp=self.f, indent=4)
+        self.f.close()
+
+    def process_item(self, item, spider):
+        """
+        #数据库请用自己的地址替换
+        con=MySQLdb.connect(host='xx.xx.xx.xx',user='xx',passwd='xx',db='xx',port=xx,charset='utf8')
+        cur=con.cursor()
+        cur.execute('insert into Tag values(%s,%s)',("形式",item['style']))
+        con.commit()
+        con.close()
+        print("style",item['style'])
+        self.content.append(item)
+        """
+        return item
+
+
 class AuthorPipline(object):
-	content=[]
-	def open_spider(self,spider):
-		self.f=open("author.json",'w')
-	def close_spider(self,spider):
-		json.dump(self.content,fp=self.f,indent=4)
-		self.f.close()
-	def process_item(self,item,spider):
-		#con=MySQLdb.connect(host='xx.xx.xx.xx',user='xx',passwd='xx',db='xx',port=xx,charset='utf8')
-		#cur=con.cursor()
-		#cur.execute('insert into author values(%s,%s,%s)',(item['name'],item['img'],item['detail']))
-		#con.commit()
-		#con.close()
-		#self.content.append(item)
-		return item
+    content = []
+
+    def open_spider(self, spider):
+        self.f = open("author.json", 'w')
+
+    def close_spider(self, spider):
+        json.dump(self.content, fp=self.f, indent=4)
+        self.f.close()
+
+    def process_item(self, item, spider):
+        # con=MySQLdb.connect(host='xx.xx.xx.xx',user='xx',passwd='xx',db='xx',port=xx,charset='utf8')
+        # cur=con.cursor()
+        # cur.execute('insert into author values(%s,%s,%s)',(item['name'],item['img'],item['detail']))
+        # con.commit()
+        # con.close()
+        # self.content.append(item)
+        return item
+
+
 class PoetPipline(object):
-	content=[]
-	def open_spider(self,spider):
-		self.f=open("poet.json",'w')
-	def close_spider(self,spider):
-		json.dump(self.content,fp=self.f,indent=4)
-		self.f.close()
-	def process_item(self,item,spider):
-		con=MySQLdb.connect(host='xx.xx.xx.xx',user='xx',passwd='xx',db='xx',port=xx,charset='utf8')
-		cur=con.cursor()
-		cur.execute('insert into poet values(%s,%s,%s,%s,%s,%s,%s,%s,%s)',(item['name'],item['dynasty'],item['author'],item['content'],item['tag'],item['fanyi'],item['zhushi'],item['cankao'],item['shangxi']))
-		con.commit()
-		con.close()
-		self.content.append(item)
-		print("nNNNNNNNNN",item["n"])
-		return item
+    content = []
 
+    def open_spider(self, spider):
+        self.f = open("poet.json", 'w')
 
+    def close_spider(self, spider):
+        json.dump(self.content, fp=self.f, indent=4)
+        self.f.close()
 
+    def process_item(self, item, spider):
+        con = pymysql.connect(host='xx.xx.xx.xx', user='xx', passwd='xx', db='xx', port=xx, charset='utf8')
+        cur = con.cursor()
+        cur.execute('insert into poet values(%s,%s,%s,%s,%s,%s,%s,%s,%s)', (
+        item['name'], item['dynasty'], item['author'], item['content'], item['tag'], item['fanyi'], item['zhushi'],
+        item['cankao'], item['shangxi']))
+        con.commit()
+        con.close()
+        self.content.append(item)
+        print("nNNNNNNNNN", item["n"])
+        return item
